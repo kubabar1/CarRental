ab26f3448190615cf0cf7d6726bef7ed121a0e14
kubabar1 <kubabar1@interia.pl>
2023-09-02 20:09:30 +0200

    Fix eureka server configuration

--- api/auth-service/src/main/java/com/carrental/authservice/AuthServiceApplication.java
+++ api/auth-service/src/main/java/com/carrental/authservice/AuthServiceApplication.java
@@ -7,6 +7,7 @@ import com.carrental.commons.config.SpringFoxConfig;
 import com.carrental.authservice.config.AuthServiceCoreConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+//import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Import;
 
@@ -19,6 +20,7 @@ import org.springframework.context.annotation.Import;
 })
 @SpringBootApplication
 @EnableFeignClients
+//@EnableHystrix
 public class AuthServiceApplication {
 
     public static void main(String[] args) {
